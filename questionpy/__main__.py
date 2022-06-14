d0f7a4cc5948342102321e22b2132a12ffc725f5
Maximilian Haye <m.haye@tu-berlin.de>
2022-06-14 15:57:24 +0200

    refactor: make settings type-checkable by mypy

--- questionpy/__main__.py
+++ questionpy/__main__.py
@@ -6,17 +6,22 @@ from . import __version__
 from .app import QPyServer
 from .settings import Settings
 
+_DEFAULT_CONFIG_FILES = (
+    Path('.', 'config.ini'),
+    Path('/etc/questionpy-server.ini'),
+)
 
-def main():
+
+def main() -> None:
     # Log level
     logging.basicConfig(level=logging.INFO)
 
     # Arguments
     parser = argparse.ArgumentParser(description=f"QuestionPy Application Server {__version__}")
-    parser.add_argument('--config', help='path to config file', default=None, type=Path)
+    parser.add_argument('--config', help='path to config file', default=_DEFAULT_CONFIG_FILES, type=Path)
     args = parser.parse_args()
 
-    settings = Settings(args.config)
+    settings = Settings(config_files=args.config)
 
     qpy_server = QPyServer(settings)
     qpy_server.start_server()
--- questionpy/settings.py
+++ questionpy/settings.py
@@ -1,10 +1,30 @@
 import logging
 from configparser import ConfigParser
 from pathlib import Path
-from typing import Any, Callable, Dict, List, Optional, Tuple
+from typing import Any, Callable, Dict, Tuple
 
 from pydantic import BaseModel, BaseSettings
-from pydantic.env_settings import SettingsSourceCallable
+from pydantic.env_settings import InitSettingsSource, SettingsSourceCallable
+
+
+class IniFileSettingsSource:
+    def __init__(self, config_files: Tuple[Path, ...]):
+        self.config_files = config_files
+
+    def __call__(self, settings: BaseSettings) -> Dict[str, Any]:
+        log = logging.getLogger('questionpy-server')
+        for path in self.config_files:
+            if not path.is_file():
+                log.info(f"No file found at '{path}'")
+                continue
+            log.info(f"Reading config file '{path}'")
+
+            parser = ConfigParser()
+            parser.read(path)
+            return {'webservice': dict(parser['webservice'])}
+
+        log.fatal('No config file found!')
+        return {}
 
 
 class WebserviceSettings(BaseModel):
@@ -14,50 +34,20 @@ class WebserviceSettings(BaseModel):
 
 class Settings(BaseSettings):
     webservice: WebserviceSettings
-    _config_file_paths: List[Path]
-
-    def __init__(self, config_files: Optional[List[Path]], *args, **kwargs):
-        if config_files is not None:
-            self._config_file_paths = config_files
-        else:
-            self._config_file_paths = [
-                Path('.', 'config.ini'),
-                Path('/etc/questionpy-server.ini'),
-            ]
-
-        super().__init__(*args, **kwargs)
-
-    def get_config_file_paths(self) -> List[Path]:
-        return self._config_file_paths
+    config_files: Tuple[Path, ...] = ()
 
     class Config:
         env_prefix = 'qpy_'
-        underscore_attrs_are_private = True
 
         @classmethod
         def customise_sources(
                 cls,
-                init_settings: SettingsSourceCallable,
+                init_settings: InitSettingsSource,
                 env_settings: SettingsSourceCallable,
+                # pylint: disable=unused-argument
                 file_secret_settings: SettingsSourceCallable
         ) -> Tuple[Callable, ...]:
-            return init_settings, ini_config_settings_source, env_settings
+            if "config_files" in init_settings.init_kwargs:
+                return init_settings, IniFileSettingsSource(init_settings.init_kwargs["config_files"]), env_settings
 
-
-def ini_config_settings_source(settings: Settings) -> Dict[str, Any]:
-    if not settings.get_config_file_paths():
-        return {}
-
-    log = logging.getLogger('questionpy-server')
-    for path in settings.get_config_file_paths():
-        if not path.is_file():
-            log.info(f"No file found at '{path}'")
-            continue
-        log.info(f"Reading config file '{path}'")
-
-        parser = ConfigParser()
-        parser.read(path)
-        return {'webservice': dict(parser['webservice'])}
-
-    log.fatal('No config file found!')
-    return {}
+            return init_settings, env_settings
