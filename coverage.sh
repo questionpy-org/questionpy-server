4d8e6d572be29d6f89289769a265d04b7017c0b6
Alexander Schmitz <alexander.schmitz@tu-berlin.de>
2022-06-21 14:38:46 +0200

    refactor: shell script for ci tools

--- coverage.sh
+++ coverage.sh
@@ -1,16 +1,13 @@
 #!/bin/sh
 
-printf -- 'running flake8 \n';
-flake8 questionpy
-flake8 tests
+printf -- 'running flake8 \n'
+flake8 questionpy tests
 
-printf -- 'running pylint \n';
-pylint questionpy
-pylint tests
+printf -- 'running pylint \n'
+pylint questionpy tests
 
-printf -- 'running pytest \n';
+printf -- 'running pytest \n'
 pytest tests
 
-printf -- 'running mypy \n';
-mypy questionpy
-mypy tests
+printf -- 'running mypy \n'
+mypy questionpy tests
