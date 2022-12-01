e117efbea36269ee98154d6061c3a1ff4b1914ff
Jan Britz <britz@tu-berlin.de>
2022-12-01 13:13:13 +0100

    refactor: use contextmanager when mocking a method

--- tests/collector/test_indexer.py
+++ tests/collector/test_indexer.py
@@ -1,4 +1,4 @@
-from unittest.mock import AsyncMock, patch, Mock
+from unittest.mock import patch, Mock
 
 from _pytest.tmpdir import TempPathFactory
 
@@ -11,19 +11,19 @@ from tests.conftest import PACKAGES
 
 async def test_update_gets_called_on_each_get_call(tmp_path_factory: TempPathFactory) -> None:
     indexer = Indexer([])
-    indexer.update = AsyncMock()
 
-    await indexer.get_packages()
-    assert indexer.update.call_count == 1
+    with patch.object(indexer, 'update') as update:
+        await indexer.get_packages()
+        assert update.call_count == 1
 
-    await indexer.get_by_name('test')
-    assert indexer.update.call_count == 2
+        await indexer.get_by_name('test')
+        assert update.call_count == 2
 
-    await indexer.get_by_name_and_version('test', '1.0.0')
-    assert indexer.update.call_count == 3
+        await indexer.get_by_name_and_version('test', '1.0.0')
+        assert update.call_count == 3
 
-    await indexer.get_by_hash('test')
-    assert indexer.update.call_count == 4
+        await indexer.get_by_hash('test')
+        assert update.call_count == 4
 
 
 async def test_update_calls_get_packages(tmp_path_factory: TempPathFactory) -> None:
@@ -35,18 +35,18 @@ async def test_update_calls_get_packages(tmp_path_factory: TempPathFactory) -> N
     indexer = Indexer([local_collector])
 
     # Check if update calls get_packages.
-    local_collector.get_packages = AsyncMock()
-    await indexer.update(force=True)
-    local_collector.get_packages.assert_called_once()
+    with patch.object(local_collector, 'get_packages') as get_packages:
+        await indexer.update(force=True)
+        get_packages.assert_called_once()
 
 
 def test_register_packages() -> None:
     # The method register_packages should call register_package for each package.
     packages = [Mock() for _ in range(3)]
     indexer = Indexer([])
-    indexer.register_package = Mock()
-    indexer.register_packages(packages)
-    assert indexer.register_package.call_count == 3
+    with patch.object(indexer, 'register_package') as register_package:
+        indexer.register_packages(packages)
+        assert register_package.call_count == 3
 
 
 async def test_local_or_repo_packages(tmp_path_factory: TempPathFactory) -> None:
