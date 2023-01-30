2e3bac64525a805966292b75ebcfccd7316faeb5
Jan Britz <britz@tu-berlin.de>
2023-01-30 09:06:06 +0100

    refactor: use pydantics builtin `ByteSize` class instead of `Size`

--- questionpy_common/constants.py
+++ questionpy_common/constants.py
@@ -1,7 +1,12 @@
 from typing import Final
 
-from questionpy_common.misc import Size, SizeUnit
+from pydantic import ByteSize
+
+
+# General.
+KiB: Final[int] = 1024  # pylint: disable=invalid-name
+MiB: Final[int] = 1024 * KiB  # pylint: disable=invalid-name
 
 # Request.
-MAX_BYTES_PACKAGE: Final[Size] = Size(20, SizeUnit.MiB)
-MAX_BYTES_QUESTION_STATE: Final[Size] = Size(2, SizeUnit.MiB)
+MAX_PACKAGE_SIZE: Final[ByteSize] = ByteSize(20 * MiB)
+MAX_QUESTION_STATE_SIZE: Final[ByteSize] = ByteSize(2 * MiB)
