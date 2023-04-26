e0272410530d4f5d6a26af8d4c13d726b47df09a
Jan Britz <britz@tu-berlin.de>
2023-04-26 13:52:43 +0200

    feat: add docstrings and rename `version`
    to `repository_schema_version`

--- questionpy_server/repository/models.py
+++ questionpy_server/repository/models.py
@@ -7,20 +7,31 @@ from questionpy_server.utils.manfiest import ComparableManifest, SemVer
 
 
 class RepoMeta(BaseModel):
+    """Metadata of the repository."""
+    repository_schema_version: int
+    """Version of the repository index schema."""
     timestamp: datetime
+    """Timestamp of the repository index creation."""
     size: PositiveInt
+    """Size of the compressed repository index in bytes."""
     sha256: str
-    version: int
+    """SHA256 hash of the compressed repository index."""
 
 
 @total_ordering
 class RepoPackageVersion(BaseModel):
+    """Represents a specific version of a package in the repository."""
     version: SemVer
+    """Version of the package."""
     api_version: str
+    """Compatible API version of the package."""
 
     path: str
+    """Relative path to the package inside the repository."""
     size: PositiveInt
+    """Size of the package in bytes."""
     sha256: str
+    """SHA256 hash of the package."""
 
     def __lt__(self, other: object) -> bool:
         if not isinstance(other, RepoPackageVersion):
@@ -34,24 +45,41 @@ class RepoPackageVersion(BaseModel):
 
 
 class RepoPackageVersions(BaseModel):
+    """Represents a package with all its versions in the repository."""
     manifest: ComparableManifest
+    """Manifest of the most recent version of the package."""
     versions: list[RepoPackageVersion]
+    """List of all versions of the package."""
 
 
 class RepoPackageIndex(BaseModel):
+    """Represents the index of the repository."""
     packages: list[RepoPackageVersions]
+    """List of all available packages in the repository."""
 
 
 @dataclass
 class RepoPackage:
+    """Represents a package in the repository."""
     manifest: ComparableManifest
+    """Manifest of the package."""
 
     path: str
+    """Path to the package inside the repository."""
     size: PositiveInt
+    """Size of the package in bytes."""
     sha256: str
+    """SHA256 hash of the package."""
 
     @classmethod
     def combine(cls, manifest: ComparableManifest, repo_package_version: RepoPackageVersion) -> 'RepoPackage':
+        """
+        Combines the manifest of a package with a specific version of that package.
+
+        Args:
+            manifest: manifest of the most recent version of the package
+            repo_package_version: version of the package
+        """
         # Replace package version and api version with actual versions.
         modified_manifest = manifest.copy(deep=True)
         modified_manifest.version = repo_package_version.version
