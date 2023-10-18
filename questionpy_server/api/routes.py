26a924745c2d88d6c90078999154bacb134ce7c9
Maximilian Haye <m.haye@tu-berlin.de>
2023-10-18 17:07:01 +0200

    wip: poc loading of static package files

--- questionpy_server/api/routes.py
+++ questionpy_server/api/routes.py
@@ -1,16 +1,20 @@
 #  This file is part of the QuestionPy Server. (https://questionpy.org)
 #  The QuestionPy Server is free software released under terms of the MIT license. See LICENSE.md.
 #  (c) Technische Universität Berlin, innoCampus <info@isis.tu-berlin.de>
-
-from typing import TYPE_CHECKING, Optional
+import asyncio
+from asyncio import to_thread
+from pathlib import Path
+from typing import TYPE_CHECKING, Optional, Literal
+from zipfile import ZipFile
 
 from aiohttp import web
 from aiohttp.web_exceptions import HTTPMethodNotAllowed, HTTPNotFound
+from pydantic import BaseModel
 
 from questionpy_server.factories import AttemptScoredFactory
 from questionpy_server.web import ensure_package_and_question_state_exist, json_response
 from .models import AttemptStartArguments, AttemptScoreArguments, AttemptViewArguments, \
-    QuestionCreateArguments, QuestionEditFormResponse, RequestBaseData
+    QuestionCreateArguments, QuestionEditFormResponse, RequestBaseData, MainBaseModel
 from ..package import Package
 from ..worker.worker import Worker
 
@@ -113,6 +117,38 @@ async def post_question(request: web.Request, data: QuestionCreateArguments,
 @routes.post(r'/packages/{package_hash:\w+}/question/migrate')
 async def post_question_migrate(_request: web.Request) -> web.Response:
     raise HTTPMethodNotAllowed("", "")
+
+
+class LoadFileArgument(MainBaseModel):
+    kind: Literal["static"]
+    path: str
+
+
+def load_static_package_file_sync(loop: asyncio.AbstractEventLoop, package: Path, path: str,
+                                  target: web.StreamResponse) -> None:
+    with ZipFile(package) as package_file:
+        with package_file.open(path) as static_file:
+            while True:
+                chunk = static_file.read(512 * 1024)
+                if not chunk:
+                    loop.run_until_complete(target.write_eof())
+                    return
+
+                loop.run_until_complete(target.write(chunk))
+
+
+@routes.post(r'/packages/{package_hash:\w+}/load-file')  # type: ignore[arg-type]
+@ensure_package_and_question_state_exist
+async def post_load_file(_request: web.Request, data: LoadFileArgument,
+                         package: Package, _question_state: Optional[bytes] = None) -> web.Response:
+    package_path = await package.get_path()
+    if data.kind == "static":
+        response = web.Response()
+        await to_thread(lambda: load_static_package_file_sync(asyncio.get_running_loop(), package_path,
+                                                              data.path, response))
+        return response
+
+    raise web.HTTPUnprocessableEntity()
 
 
 @routes.post(r'/package-extract-info')  # type: ignore[arg-type]
