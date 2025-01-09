f05b5e85eb2544281813fbbe72af7ab51b1aabcd
Jan Britz <britz@tu-berlin.de>
2025-01-09 13:01:36 +0100

    refactor: move `factories.py` to test folder

--- questionpy_common/dev/__init__.py
+++
@@ -1,3 +0,0 @@
-#  This file is part of QuestionPy. (https://questionpy.org)
-#  QuestionPy is free software released under terms of the MIT license. See LICENSE.md.
-#  (c) Technische Universität Berlin, innoCampus <info@isis.tu-berlin.de>
