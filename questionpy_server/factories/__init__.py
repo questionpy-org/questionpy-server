35402286fb13c396bc584116335529aa2e056cbb
Jan Britz <britz@tu-berlin.de>
2025-03-17 11:37:19 +0100

    refactor: removal of redundant factories

--- questionpy_server/factories/__init__.py
+++
@@ -1,9 +0,0 @@
-#  This file is part of the QuestionPy Server. (https://questionpy.org)
-#  The QuestionPy Server is free software released under terms of the MIT license. See LICENSE.md.
-#  (c) Technische Universität Berlin, innoCampus <info@isis.tu-berlin.de>
-
-from .attempt import AttemptScoredFactory
-from .package import PackageVersionInfoFactory
-from .question_state import RequestBaseDataFactory
-
-__all__ = ["AttemptScoredFactory", "PackageVersionInfoFactory", "RequestBaseDataFactory"]
