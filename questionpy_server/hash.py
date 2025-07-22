1d34ef409d1a647b6cea3456853a54cab1df3f96
Maximilian Haye <m.haye@tu-berlin.de>
2025-07-22 17:43:47 +0200

    refactor: use file_digest in calculate_hash
    Also supports already-opened files, to be used by the SDK.

--- questionpy_server/hash.py
+++ questionpy_server/hash.py
@@ -1,31 +1,26 @@
 #  This file is part of the QuestionPy Server. (https://questionpy.org)
 #  The QuestionPy Server is free software released under terms of the MIT license. See LICENSE.md.
 #  (c) Technische Universität Berlin, innoCampus <info@isis.tu-berlin.de>
-
-from hashlib import sha256
+from hashlib import file_digest, sha256
+from io import BufferedIOBase, RawIOBase
 from pathlib import Path
 from typing import NamedTuple
 
-from questionpy_common.constants import MiB
 
-
-def calculate_hash(source: bytes | Path) -> str:
+def calculate_hash(source: bytes | Path | RawIOBase | BufferedIOBase) -> str:
     """Calculates the sha256 of either bytes or a file.
 
     Args:
-        source (Union[bytes, Path]) : bytes or path to file
-
-    Returns:
-        str: the sha256
+        source: bytes, already opened file, or path to a file
     """
-    sha = sha256()
-
     if isinstance(source, bytes):
+        sha = sha256()
         sha.update(source)
-    else:
+    elif isinstance(source, Path):
         with source.open("rb") as file:
-            while chunk := file.read(5 * MiB):
-                sha.update(chunk)
+            sha = file_digest(file, sha256)
+    else:
+        sha = file_digest(source, sha256)
 
     return sha.hexdigest()
 
